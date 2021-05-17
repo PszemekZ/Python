3776379c3dacf01d6ad39d035d9bfa8fa4cb6da7
przemyslawZabielski <przemyslaw.zabielski1@gmail.com>
2021-05-17 18:46:15 +0200

    change thread

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from random import randint
 import tkinter as tk
 import threading
+from time import sleep
 
-root = tk.Tk()
 
-def tread():
-    threading.Timer(5.0, tread).start()
-    arr = [randint(1,100)]
-    label = tk.Label(root, text=arr)
+def change_text(continue_running, label):
+    while continue_running.is_set():
+        label['text'] = randint(1,100)
+        sleep(30)
+
+
+def main():
+    root = tk.Tk()
+
+    label = tk.Label(root, text='')
+
+    continue_thread = threading.Event()
+    continue_thread.set()
+    change_text_thread = threading.Thread(target=change_text, kwargs={'continue_running': continue_thread, 'label': label})
+    change_text_thread.start()
+
     label.pack()
-     
-tread()
-root.mainloop()   
+    root.mainloop()
+
+    continue_thread.clear()
+    change_text_thread.join()
 
 
+if __name__ == '__main__':
+    main()
